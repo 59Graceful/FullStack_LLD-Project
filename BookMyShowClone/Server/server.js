78642198d1d33951bc2e224e4f088399053ce80f
59Graceful <73896035+59Graceful@users.noreply.github.com>
2026-01-02 00:50:30 +0530

    Theatre APIs

--- BookMyShowClone/Server/server.js
+++ BookMyShowClone/Server/server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors')
 const userRouter = require("./routes/userRouter");
-const movieRoute = require('./routes/movieRoute')
+const movieRoute = require('./routes/movieRoute');
+const theatreRoute = require('./routes/theatreRoute');
 const dbConfig = require("./config/dbconfig");
 app.use(cors())
 app.use(express.json())
 app.use('/api/users',userRouter);
-app.use('/api/movies', movieRoute)
+app.use('/api/movies', movieRoute);
+app.use('/api/theatres',theatreRoute);
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is listening to port no ${process.env.PORT}`)
